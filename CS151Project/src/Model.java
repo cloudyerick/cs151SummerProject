8627d2e58e5d8e59d0921ff2af887cba7026d1b4
rasenjon <nahtanoj222@yahoo.com>
2017-07-26 16:59:55 -0700

    Model Methods
    Added some calendar mutator methods

--- CS151Project/src/Model.java
+++ CS151Project/src/Model.java
@@ -1,4 +1,31 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Model {
 
+	private GregorianCalendar cal = new GregorianCalendar();
+	
+	public Model() {
+		
+	}
+	
+	//GOES TO NEXT DAY - JONATHAN
+	public void nextDay() {
+		cal.add(Calendar.DAY_OF_MONTH, 1);
+	}
+	
+	//GOES TO PREVIOUS DAY - JONATHAN 
+	public void previousDay() {
+		cal.add(Calendar.DAY_OF_MONTH, -1);
+	}
+	
+	//GOES TO NEXT DAY - JONATHAN
+	public void nextMonth() {
+		cal.add(Calendar.MONTH, 1);
+	}
+	
+	//GOES TO PREVIOUS DAY - JONATHAN 
+	public void previousMonth() {
+		cal.add(Calendar.MONTH, -1);
+	}
 }
