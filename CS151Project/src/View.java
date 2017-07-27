48c682d77cd98df3325389e275f28c9a01bf5127
rasenjon <nahtanoj222@yahoo.com>
2017-07-26 17:50:11 -0700

    Added more GUI

--- CS151Project/src/View.java
+++ CS151Project/src/View.java
@@ -1,5 +1,6 @@
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -18,14 +19,27 @@ public class View extends JFrame
 		JButton left = new JButton("<");
 		JButton right = new JButton(">");
 		
+		JButton dayButton = new JButton("Day");
+		JButton weekButton = new JButton("Week");
+		JButton monthButton = new JButton("Month");
+		JButton agendaButton = new JButton("Agenda");
+		
 		JPanel topLeft = new JPanel();
+		JPanel topRight = new JPanel();
 		
 		topLeft.add(today);
 		topLeft.add(left);
 		topLeft.add(right);
 		
-		myFrame.add(topLeft);
+		topRight.add(dayButton);
+		topRight.add(weekButton);
+		topRight.add(monthButton);
+		topRight.add(agendaButton);
 		
+		myFrame.add(topLeft);
+		myFrame.add(topRight);
+		
+		myFrame.setLayout(new FlowLayout());  //Experimental Layout (for now)
 		myFrame.setVisible(true);
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
